61ce4e665ac20fddb6976890f43c6477dc95cc98
Angor78 <angor78@gmail.com>
2022-04-29 18:14:54 +0300

    map()

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+type NewComponentType = {
+  topCars: TopCarsType[]
+}
+type TopCarsType = {
+  key:number
+  manufacturer: string
+  model: string
+}
+
 function App() {
+  const topCars = [
+    {key:1,manufacturer: 'BMW', model: 'm5cs'},
+    {key:2,manufacturer: 'Mercedes', model: 'e63s'},
+    {key:3,manufacturer: 'Audi', model: 'rs6'}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewComponent topCars={topCars}/>
     </div>
   );
+}
+
+const NewComponent = (props: NewComponentType) => {
+  return (
+    <ul>
+      {props.topCars.map((obj,index) => {
+          return (
+            <li key={index}>
+              <span> Manufactured: {obj.manufacturer}</span>
+              <span> Model: {obj.model}</span>
+            </li>)
+        }
+      )}
+    </ul>
+
+    // <table>
+    //   <tr>
+    //     <th>Company</th>
+    //     <th>Model</th>
+    //   </tr>
+    //   {props.topCars.map((obj, index) => {
+    //     return (
+    //       <tr>
+    //         <th key={index}>{obj.manufacturer}</th>
+    //         <th key={index}>{obj.model}</th>
+    //       </tr>
+    //     )
+    //   })}
+    // </table>
+  )
 }
 
 export default App;

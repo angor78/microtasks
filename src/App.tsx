79dbd451c75c66bb9d1b8baf9db9b507ede9a12b
Angor78 <angor78@gmail.com>
2022-04-30 08:05:34 +0300

    button callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,27 @@
 import React from 'react';
 import './App.css';
+import {Button} from "./component/Button/Button";
 
-
-type NewComponentType = {
-  topCars: TopCarsType[]
-}
-type TopCarsType = {
-  key:number
-  manufacturer: string
-  model: string
-}
 
 function App() {
-  const topCars = [
-    {key:1,manufacturer: 'BMW', model: 'm5cs'},
-    {key:2,manufacturer: 'Mercedes', model: 'e63s'},
-    {key:3,manufacturer: 'Audi', model: 'rs6'}
-  ]
+  const Button1Foo = (subscriber: string, age: number) => {
+    console.log(subscriber, age)
+  }
+  const Button2Foo = (subscriber: string, age: number) => {
+    console.log(subscriber, age)
+  }
+  const Button3Foo = () => {
+    console.log("Stupid")
+  }
   return (
     <div className="App">
-      <NewComponent topCars={topCars}/>
+      <Button name={'Button 1'} callBack={() => Button1Foo("Ivan", 23)}/>
+      <Button name={'Button 2'} callBack={() => Button2Foo("Vasia", 45)}/>
+      <Button name={'Button 3'} callBack={Button3Foo}/>
     </div>
   );
 }
 
-const NewComponent = (props: NewComponentType) => {
-  return (
-    <ul>
-      {props.topCars.map((obj,index) => {
-          return (
-            <li key={index}>
-              <span> Manufactured: {obj.manufacturer}</span>
-              <span> Model: {obj.model}</span>
-            </li>)
-        }
-      )}
-    </ul>
-
-    // <table>
-    //   <tr>
-    //     <th>Company</th>
-    //     <th>Model</th>
-    //   </tr>
-    //   {props.topCars.map((obj, index) => {
-    //     return (
-    //       <tr>
-    //         <th key={index}>{obj.manufacturer}</th>
-    //         <th key={index}>{obj.model}</th>
-    //       </tr>
-    //     )
-    //   })}
-    // </table>
-  )
-}
 
 export default App;
+
